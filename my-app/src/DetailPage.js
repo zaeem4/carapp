c204ec68c0ac2b8678d62665d269d70c55eb8643
zaeem4 <zaeem1169@gmail.com>
2023-06-24 15:32:04 +0500

    REVISION 8 - bug fix

--- my-app/src/DetailPage.js
+++ my-app/src/DetailPage.js
@@ -89,7 +89,7 @@ export default function DetailPage() {
     } else {
       getDetails(_id);
     }
-  }, []);
+  }, [_id]);
 
   const handleUpdatedValue = (event) => {
     setUpdatedValue(event.target.value);
